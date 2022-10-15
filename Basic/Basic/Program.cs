3ae41a4c16b6ddbaf57dbee81c786268aff6d023
lrf <465197183@qq.com>
2022-10-15 09:50:26 +0800

    C#基础语法学习

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(typeof(String) == typeof(string));
+            //TestVar();
+            //int val = 0;
+            //long val2 = 0;
+            //double val3 = 0;
+            //float val4 = 0;
+            //Console.WriteLine(val.GetType());
+            //Console.WriteLine(val2.GetType());
+            //Console.WriteLine(val3.GetType());  
+            //Console.WriteLine(val4.GetType());
+            //Console.WriteLine(val.GetType() == typeof(int));
+            //PrintDataType();
+            //UseOperator();
+            //UserExpression();
+            TestForEach();
+            Console.ReadKey();
+        }
+
+        
+        static void TestForEach()
+        {
+            var IntValues = new List<int>() { 1, 3, 5, 7, 9 };
+            foreach(var value in IntValues)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        static void TestVar()
+        {
+            var a = 100;
+            var s = "hello";
+            var c = 3.1415926;
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(s.GetType());
+            Console.WriteLine(c.GetType());
+        }
+        static void PrintDataType()
+        {
+            int intVal = 100;
+            long longVal = 100L;
+            double doubleVal = 100.5d;
+            float floatVal = 3.14f;
+
+            Console.WriteLine("{0},{1}", sizeof(double), sizeof(long));
+
+            longVal = intVal;
+            doubleVal = intVal;
+            doubleVal = longVal;
+            intVal = (int)floatVal;
+            doubleVal = (double)longVal;
+        }
+
+        static void StringAndNumberType()
+        {
+            //字符串转数字
+            string strValue = "100";
+            int intVal = int.Parse(strValue);   
+            double doubleVal = double.Parse(strValue);
+            intVal = Convert.ToInt32(strValue);
+            doubleVal = Convert.ToDouble(strValue);
+
+            //数字转字符串
+            strValue = intVal.ToString();
+            strValue = doubleVal.ToString();
+            strValue = 100.ToString();
+
+            strValue = intVal + "";
+        }
+
+        static void UseOperator()
+        {
+            // + - * / ()
+            //double result = ((5 + 3) * 2 - 7) / 6.0;
+            //Console.WriteLine(result);
+
+            //int val = 9 % 4;
+            //Console.WriteLine(val);
+
+            int val2 = 1;
+            Console.WriteLine(val2++); 
+            int val3 = 1;
+            Console.WriteLine(++val3);
+        }
+        static void UserExpression()
+        {
+            int value = 100;
+            Console.WriteLine(value*2);
+        }
+    }
+}
---
+++ SayHello/SayHello/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestConsoleProperty();
+            Console.WriteLine("Hello!");
+            TestInputAndOutput();
+            Console.WriteLine("First ReadKey()");
+            Console.ReadKey();
+            Console.WriteLine("Seccond ReadKey()");
+            Console.ReadKey(true); //不希望用户的按键显示在控制台上
+            Console.WriteLine("Final Readkey()");
+            Console.ReadKey();
+            Console.Beep();
+            Console.WriteLine("Press any key to qui...");
+            Console.ReadKey();
+        }
+        
+        static void TestConsoleProperty()
+        {
+            Console.Title = "Current Time:" + DateTime.Now;
+            Console.ForegroundColor=ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+
+        static void TestInputAndOutput()
+        {
+            Console.Write("Please input a string:");
+            String userInfo = Console.ReadLine();
+            
+
+            Console.WriteLine("User input:{0} length:{1}",userInfo,userInfo.Length);
+        }
+    }
+}
